00c074a921f69f6f2b16a66cf8bbe138d8ca6189
ibrahim5253 <cs14btech11041@iith.ac.in>
2017-03-13 20:50:34 +0530

    Added explanation to Sampler.lua

--- Sampler.lua
+++ Sampler.lua
@@ -11,11 +11,15 @@ function Sampler:__init()
 end 
 
 function Sampler:updateOutput(input)
+    -- input[1] -> mean (mu)
+    -- input[2] -> log_var (log sigma^2)
+    -- reparameterization trick: x = mu + sigma * eps
     self.eps = self.eps or input[1].new()
     self.eps:resizeAs(input[1]):copy(torch.randn(input[1]:size()))
 
     self.output = self.output or self.output.new()
     self.output:resizeAs(input[2]):copy(input[2])
+    -- sigma = exp(0.5*log_var)
     self.output:mul(0.5):exp():cmul(self.eps)
 
     self.output:add(input[1])
@@ -24,6 +28,10 @@ function Sampler:updateOutput(input)
 end
 
 function Sampler:updateGradInput(input, gradOutput)
+    -- let y = log_var
+    -- x = mu + exp(0.5y)*eps
+    -- dC/dmu = dC/dx -> gradInput[1]
+    -- dC/dy  = exp(0.5y) * 0.5 * eps * dC/dx -> gradInput[2]
     self.gradInput[1] = self.gradInput[1] or input[1].new()
     self.gradInput[1]:resizeAs(gradOutput):copy(gradOutput)
     
